9dfcb1b7ea7cf494f87cdd37454a3ad4fba4f848
VolVox99 <alirastegar123@gmail.com>
2020-10-03 16:19:12 -0700

    Updates

---
+++ precache-manifest.d40d16a9ff67e68b2654a5819d57e453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65c1cbc3be9afd32106e58ee7a6161b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c60cbc25d08c92d537b5",
+    "url": "/static/css/main.1f253a37.chunk.css"
+  },
+  {
+    "revision": "a935b266168521b76b65",
+    "url": "/static/js/2.626cfcca.chunk.js"
+  },
+  {
+    "revision": "718fa98a469a94012f350cb6e7e01f06",
+    "url": "/static/js/2.626cfcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60cbc25d08c92d537b5",
+    "url": "/static/js/main.4aa57643.chunk.js"
+  },
+  {
+    "revision": "a54796945fe15188ddea",
+    "url": "/static/js/runtime-main.ef5f4bae.js"
+  }
+]);
